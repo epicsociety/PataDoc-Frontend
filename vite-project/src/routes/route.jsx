53b0d204af90a7e211536459328b96348cec9264
Epicsociety <measkswho@gmail.com>
2024-01-06 09:53:19 +0300

    add mobile design in public folder

--- vite-project/src/routes/route.jsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react";
-
-const Home = lazy(() => import("../pages/home"));
-const Error = lazy(() => import("../components/error-page"));
-const AllReports = lazy(() => import("../pages/all-reports"));
-const Reports = lazy(() => import("../pages/report"));
-const About = lazy(() => import("../pages/about"));
-
-export { Home, Error, AllReports, Reports, About };
